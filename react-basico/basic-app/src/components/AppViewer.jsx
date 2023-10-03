01245f2f3ad2bde69d1060b09fdca3f75a0afeed
liviafioraneli <liviafioraneli@hotmail.com>
2023-10-03 12:31:12 -0300

    Aula0310 Componentes e Funções

--- react-basico/basic-app/src/components/AppViewer.jsx
+++ react-basico/basic-app/src/components/AppViewer.jsx
@@ -1,7 +1,17 @@
 import styles from './AppViewer.module.css'
+//Camada de compilação de serviços
+function somaNumero (n1, n2){
+  return (n1+n2)
+}
 const AppViewer = () => {
+  //retorno no display
   return (
+    <>
     <div className={styles.avContainer}>AppViewer</div>
+    <p>
+      {somaNumero(5,17)}
+    </p>
+    </>
   )
 }
 
